a655d97fa1590cb13715d52a764d43aa640e4e1b
Luis Henrique Machado <lhfam97@gmail.com>
2019-12-21 08:07:14 -0300

    Express changes

--- app.js
+++ app.js
@@ -1,9 +1,26 @@
-const http = require('http');
+const express = require('express');
 
-const routes = require('./routes');
+const bodyParser = require('body-parser');
 
-console.log(routes.someText);
+const app = express();
 
-const server = http.createServer(routes.handler);
+app.use(bodyParser.urlencoded({extended: false}));
 
-server.listen(3000); 
+app.use('/add-product', (req, res, next) => {
+    // res.send('<h1> Hello from Another product! </h1>');
+    res.send('<form action="/product" method ="POST"> <input type="text" name="title"><button type="submit">Add Product</button></form> ');
+});
+
+app.use('/product', (req, res, next) => {
+    console.log(req.body);
+    res.redirect('/');
+});
+app.use('/', (req, res, next) => {
+    console.log("Another middleware");
+    res.send('<h1> Hello from express! </h1>');
+});
+
+app.listen(3000);
+// const server = http.createServer(app);
+
+// server.listen(3000); 
